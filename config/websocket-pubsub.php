4e03938402efec0eba78596c8e8db26a73c8d4ea
Olexandr <olexandr.dolbin@gmail.com>
2022-07-10 04:14:00 +0300

    Update websocket-pubsub.php

--- config/websocket-pubsub.php
+++ config/websocket-pubsub.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'websocket_uri' => env("JBWC_WEBSOCKET_URI", 'ws://localhost:9000/'),
+    'websocket_uri' => env("JB_WEBSOCKET_URI", 'ws://localhost:9000/'),
     'options' => [
         
     ]
